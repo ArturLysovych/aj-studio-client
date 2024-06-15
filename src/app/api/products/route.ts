4b31abd628f4ca553444d5acdb22419cdedf4601
ArturLysovych <arturlisovic@gmail.com>
2024-06-15 11:59:19 +0300

    Delete route.ts

--- src/app/api/products/route.ts
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-import { NextResponse } from 'next/server';
-
-export async function GET(req: Request) {
-    const response = await axios.get('http://localhost:5000/products/');
-    const products = response.data;
-    return NextResponse.json(products);
-}
